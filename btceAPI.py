cd815727064f45f81f7781e9aafb9b5f51116c5d
alexstuk <33109346+alexstuk@users.noreply.github.com>
2021-03-02 18:36:36 -0800

    Update btceAPI.py

--- btceAPI.py
+++ btceAPI.py
@@ -1,5 +1,3 @@
-# secret: 78a98c203628379112a1613979556ca2b4ca6cfdd79ad0e86509bcf1a5c28883
-# key: HQB2602V-PQSCON7D-XO85V1L8-ECK26J40-4CC57L0M
 import http.client
 import urllib.request, urllib.parse, urllib.error
 import json
@@ -14,19 +12,23 @@ class api:
 	__wait_for_nonce = False
 
 	def __init__(self, api_key, api_secret, wait_for_nonce=False):
+		# Create an object with authentication information.
 		self.__api_key = api_key
 		self.__api_secret = api_secret
 		self.__wait_for_nonce = wait_for_nonce
 
 	def __nonce(self):
 		if self.__wait_for_nonce: time.sleep(0.001)
+		# Setting nonce using time
 		self.__nonce_v = str((time.time() - 1469674815) * 1000).split('.')[0]
 
 	def __signature(self, params):
+		# Making signature from api secret and parameters by hashing
 		sig = hmac.new(self.__api_secret.encode(), params.encode(), hashlib.sha512)
 		return sig.hexdigest()
 
 	def __api_call(self, method, params):
+		# Making any API call
 		self.__nonce()
 		params['method'] = method
 		params['nonce'] = str(self.__nonce_v)
@@ -42,6 +44,7 @@ class api:
 		return data
 
 	def get_param(self, couple, param):
+		# API call to get parameters
 		conn = http.client.HTTPSConnection("btc-e.com")
 		conn.request("GET", "/api/2/" + couple + "/" + param)
 		response = conn.getresponse().read().decode()
@@ -50,9 +53,11 @@ class api:
 		return data
 
 	def getInfo(self):
+		# API call to get info
 		return self.__api_call('getInfo', {})
 
-	def TransHistory(self, tfrom, tcount, tfrom_id, tend_id, torder, tsince, tend):
+	def trans_history(self, tfrom, tcount, tfrom_id, tend_id, torder, tsince, tend):
+		# API call to get transaction history
 		params = {
 			"from": tfrom,
 			"count": tcount,
@@ -61,9 +66,10 @@ class api:
 			"order": torder,
 			"since": tsince,
 			"end": tend}
-		return self.__api_call('TransHistory', params)
+		return self.__api_call('trans_history', params)
 
-	def TradeHistory(self, tfrom, tcount, tfrom_id, tend_id, torder, tsince, tend, tpair):
+	def trade_history(self, tfrom, tcount, tfrom_id, tend_id, torder, tsince, tend, tpair):
+		# API call to get trade history
 		params = {
 			"from": tfrom,
 			"count": tcount,
@@ -73,45 +79,27 @@ class api:
 			"since": tsince,
 			"end": tend,
 			"pair": tpair}
-		return self.__api_call('TradeHistory', params)
+		return self.__api_call('trade_history', params)
 
-	def ActiveOrders(self, tpair):
+	def active_orders(self, tpair):
+		# API call to get active orders
 		params = {"pair": tpair}
-		return self.__api_call('ActiveOrders', params)
+		return self.__api_call('active_orders', params)
 
-	def Trade(self, tpair, ttype, trate, tamount):
+	def trade(self, tpair, ttype, trate, tamount):
+		# API call to trade
 		params = {
 			"pair": tpair,
 			"type": ttype,
 			"rate": trate,
 			"amount": tamount}
-		return self.__api_call('Trade', params)
+		return self.__api_call('trade', params)
 
-	def CancelOrder(self, torder_id):
+	def cancel_order(self, torder_id):
+		# API call to cancel order
 		params = {"order_id": torder_id}
-		return self.__api_call('CancelOrder', params)
+		return self.__api_call('cancel_order', params)
 
 api_key = ''
 api_secret = ''
 btce = api(api_key, api_secret, wait_for_nonce=True)
-
-
-#print(btce.getInfo())
-
-#dealDetailsB = btce.Trade('zzz_btc', 'buy', 0.000001, 10)
-#print(dealDetailsB)
-"""
-buyPriceB = float(getPrice('btc-e', 'eth_btc', 'buy')) * 1.0009
-fundsBtce = float(btce.getInfo()['return']['funds']['btc'])
-fundsBtce = fundsBtce / buyPriceB
-print(buyPriceB)
-print(fundsBtce)
-dealDetailsB = btce.Trade('eth_btc', 'buy', round(buyPriceB,5), round(fundsBtce,5))
-print(dealDetailsB)
-#print(btceApi.getInfo())
-#dealDetailsB = btce.Trade('eth_btc', 'buy', 0.01, 0.1 )
-#print(dealDetailsB)
-#activeorder =  ActiveOrders(self, tpair):
-
-#print(btceApi.getInfo()['return']['funds']['btc'])
-"""
